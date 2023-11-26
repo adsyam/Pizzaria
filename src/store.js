56ee1547731dfa7936c82649cfc47a5d4f23046a
adsyam <yambaoadrianne@gmail.com>
2023-11-26 18:40:58 +0800

    addItem function

--- src/store.js
+++ src/store.js
@@ -3,9 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice"; //export default userSlice.reducer from userSlice
 //name convention for importing a reducer
 
+import cartReducer from "./features/cart/cartSlice";
+
 const store = configureStore({
   reducer: {
     user: userReducer, // Set the user reducer
+    cart: cartReducer,
   },
 });
 
